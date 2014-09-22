0f8a2601f62e22ad22899d2e456b6fdcd9671c30
Zachary King <zaking17@gmail.com>
2014-09-21 21:53:46 -0700

    db settings

--- settings.py
+++ settings.py
@@ -21,11 +21,11 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         # Or path to database file if using sqlite3.
-        'NAME': 'djtest',
+        'NAME': 'equilibrator',
         # Not used with sqlite3.
-        'USER': 'djangouser',
+        'USER': 'zaking',
         # Not used with sqlite3.
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
